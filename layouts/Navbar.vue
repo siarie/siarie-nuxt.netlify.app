4a92d73423de19a2645bd91273ff7395dffb8578
Sri Aspari <sriaspari@gmail.com>
2019-10-06 09:26:32 +0700

    add navbar

---
+++ layouts/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar sticky">
+    <div class="container flex">
+      <nuxt-link to="/" class="brand">{{ siteTitle }}</nuxt-link>
+
+      <input id="nav" type="checkbox" class="hidden" :checked="checked" />
+      <label for="nav" class="nav-btn">
+        <i></i>
+        <i></i>
+        <i></i>
+      </label>
+      <div class="nav-wrapper">
+        <ul class="nav-list">
+          <li v-for="(data, index) in items" :key="index" class="nav-item">
+            <nuxt-link :to="data.path" class="nav-link">
+              {{
+              data.name
+              }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      siteTitle: "Sri Aspari",
+      checked: false,
+      items: [
+        // { name: "Stories", path: "/stories" },
+        { name: "About", path: "/about" }
+        // { name: "Menu3", path: "/menu3" }
+      ]
+    };
+  }
+};
+</script>
