1f19ff378fe1f8d5c8ce4745cc03fe3990e3f9bd
Sri Aspari <sriaspari@gmail.com>
2019-10-12 09:04:49 +0700

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div class="row around-xs card mt-2" style="padding: 20px;font-size: 16px;">
+      <div class="col-md-3 col-sm-3 col-xs-12">
+        <img src=//localhost:1313/img/siarie.jpg alt="Sri Aspari"
+        class="kocheng">
+      </div>
+      <div class="col-md-9 col-sm-9 col-xs-12">
+        <p>Hello World,</p>
+        <p>
+          My name is Sri Aspari, also you can call me
+          <b>siarie</b>. I am a Web Developer, especially in Frontend Web. I come from Boyolali, a little city
+          known as New Zealand Van Java.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.kocheng {
+  width: 100%;
+  border-radius: 50%;
+}
+</style>
