78d5443f90a988bb2e36808946b5a92206b50e89
Sri Aspari <sriaspari@gmail.com>
2019-10-12 23:00:33 +0700

    add project images

--- store/projects.js
+++ store/projects.js
@@ -2,27 +2,7 @@ export const state = () => ({
     all: [{
             title: "Coming Soon",
             url: "//github.com/siarie",
-            image: "https://d33wubrfki0l68.cloudfront.net/af3940bc3e87c41c5501ebf2bcbe2b1c21112d26/a753d/img/soon.webp",
-            description: "For now, Please check out my available project on my github repositories"
-        }, {
-            title: "Coming Soon",
-            url: "//github.com/siarie",
-            image: "https://d33wubrfki0l68.cloudfront.net/af3940bc3e87c41c5501ebf2bcbe2b1c21112d26/a753d/img/soon.webp",
-            description: "For now, Please check out my available project on my github repositories"
-        }, {
-            title: "Coming Soon",
-            url: "//github.com/siarie",
-            image: "https://d33wubrfki0l68.cloudfront.net/af3940bc3e87c41c5501ebf2bcbe2b1c21112d26/a753d/img/soon.webp",
-            description: "For now, Please check out my available project on my github repositories"
-        }, {
-            title: "Coming Soon",
-            url: "//github.com/siarie",
-            image: "https://d33wubrfki0l68.cloudfront.net/af3940bc3e87c41c5501ebf2bcbe2b1c21112d26/a753d/img/soon.webp",
-            description: "For now, Please check out my available project on my github repositories"
-        }, {
-            title: "Coming Soon",
-            url: "//github.com/siarie",
-            image: "https://d33wubrfki0l68.cloudfront.net/af3940bc3e87c41c5501ebf2bcbe2b1c21112d26/a753d/img/soon.webp",
+            image: "/img/projects/being-worked.webp",
             description: "For now, Please check out my available project on my github repositories"
         }
     ]
