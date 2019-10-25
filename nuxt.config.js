5c9cfc5d757fdd1c7c73e142b987c8fc06e4adb5
Sri Aspari <sriaspari@gmail.com>
2019-10-25 14:30:37 +0700

    add sitemap and rss feed

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,36 @@
-var glob = require('glob');
+import { process } from 'ipaddr.js';
+
+const glob = require('glob');
+const fs = require('fs');
+const fm = require('front-matter');
+const marked = require('marked');
 
 let files = glob.sync('*.md', { cwd: 'content' });
-
 function getSlugs(post, _) {
   let slug = post.substr(0, post.lastIndexOf('.'));
   return `/stories/${slug}`;
 }
 
+const env = {
+  productionUrl: 'https://siarie.me',
+  author: 'Sri Aspari'
+};
+
 export default {
   mode: 'universal',
+
+  env: {
+    PRODUCTION_URL: env.productionUrl,
+    AUTHOR: env.author
+  },
+
   generate: {
     routes: function() {
       return files.map(getSlugs);
     }
   },
   head: {
-    title: 'Sri Aspari | Siarie - Web Developer',
+    title: 'Siarie',
     meta: [
       { charset: 'utf-8' },
       {
@@ -23,9 +38,12 @@ export default {
         content:
           'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
       },
-      // { name: 'msapplication-TileColor', content: '#ffffff' },
-      // { name: 'msapplication-TileImage', content: '/favicons/mstile-144x144.png' },
-      { name: "author", content: "Sri Aspari" },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      {
+        name: 'msapplication-TileImage',
+        content: '/favicons/ms-icon-144x144.png'
+      },
+      { name: 'author', content: env.author },
       { name: 'theme-color', content: '#DC143C' },
       { name: 'robots', content: 'index, follow' },
       { name: 'twitter:card', content: 'summary' },
@@ -33,7 +51,8 @@ export default {
       { property: 'og:type', content: 'profile' },
       { property: 'og:updated_time', content: new Date().toISOString() }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    noscript: [{ innerHTML: 'This website requires JavaScript :)', body: true }]
   },
   loading: { color: '#DC143C' },
   pageTransition: 'page',
@@ -41,24 +60,52 @@ export default {
     '@/assets/css/flexboxgrid.min.css',
     '@/node_modules/highlight.js/styles/github.css'
   ],
-  modules: [
-    '@nuxtjs/sitemap'
-  ],
+  modules: ['@nuxtjs/feed', '@nuxtjs/sitemap'],
   sitemap: {
-    hostname: 'https://siarie.me',
-    routes: function() {
-      return files.map(getSlugs);
-    },
-    exclude: [
-      '/',
-      '/about',
-      '/stories',
-      '/projects'
-    ],
-    path: '/rss.xml',
-    gzip: true,
-    generate: false,
+    sitemap: {
+      path: '/sitemap.xml',
+      hostname: env.productionUrl,
+      cacheTime: 1000 * 60 * 15,
+      gzip: true,
+      routes: function() {
+        return files.map(getSlugs);
+      }
+    }
   },
+  feed: [
+    {
+      path: '/stories/feed.xml',
+      cacheTime: 1000 * 60 * 15,
+      type: 'atom1',
+      async create(feed) {
+        feed.options = {
+          title: env.author,
+          link: `${env.productionUrl}/stories/feed.xml`,
+          description: "siarie's personal homepage"
+        };
+
+        feed.addCategory('Personal Blog');
+
+        feed.addContributor({
+          name: env.author,
+          email: 'mail@siarie.me',
+          link: env.productionUrl
+        });
+
+        let postList = fs.readdirSync('./content')
+        postList.forEach(post => {
+          const file = fs.readFileSync(`./content/${post}`, 'utf8')
+          const content = fm(file)
+          feed.addItem({
+            title: content.attributes.title,
+            link: `${env.productionUrl}/stories/${post.slice(0, -3)}/`,
+            description: marked(content.body),
+            date: new Date(content.attributes.date)
+          })
+        })
+      }
+    }
+  ],
   build: {
     extend(config, ctx) {
       config.module.rules.push({
